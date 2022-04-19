58e58c1d493d0e30038142f2ad9c0839dae34a71
FaizanVirani <104014327+FaizanVirani@users.noreply.github.com>
2022-04-19 10:27:47 -0400

    Create MyFunctions.h

---
+++ MyFunctions.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <math.h>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <iomanip>
+#include <vector>
+using namespace std;
+
+vector<string> splitString(string, char);
+void worksheet1(string);
+void getInformation();
+void viewInformation();
+void sortInformation();
+void removeItem();
+bool validInt(string);
+bool validDouble(string);
+bool accounts();
+int randomize();
